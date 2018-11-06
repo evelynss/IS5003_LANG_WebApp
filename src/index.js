c2fa2965789200588caa68bad2370d0125468b5b
evelynss <31617674+evelynss@users.noreply.github.com>
2018-11-07 00:14:16 +0800

    v1

--- src/index.js
+++ src/index.js
@@ -4,13 +4,35 @@ import ReactDOM from "react-dom";
 import "./styles.css";
 
 function App() {
-  return (
-    <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
-    </div>
-  );
+  return <div className="App" />;
 }
 
+var bigOne = document.getElementById("bigOne");
+var dbref = firebase
+  .database()
+  .ref()
+  .child("text");
+dbref.on("value", snap => (bigOne.innerText = snap.val()));
+
+//get elements
+const preObject = document.getElementById("object");
+const ulList = document.getElementById("list");
+//create reference
+const dbRefObject = firebase
+  .database()
+  .ref()
+  .child("object");
+
+//sync object changes
+dbRefObject.on("value", snap => {
+  preObject.innerText = JSON.stringify(snap.val(), null, 3);
+});
+//sync child add
+const dbRefList = dbRefObject.child("Hobbies");
+dbRefList.on("child_added", snap => {
+  const li = document.createElement("li");
+  li.innerText = snap.val();
+  ulList.appendChild(li);
+});
 const rootElement = document.getElementById("root");
 ReactDOM.render(<App />, rootElement);
